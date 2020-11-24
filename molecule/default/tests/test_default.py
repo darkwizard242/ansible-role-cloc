a1c1d8085beec06f6070f8569656be244f0e2271
darkwizard242 <am900820@gmail.com>
2020-11-24 10:38:18 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,16 +11,28 @@ CLOC_BINARY_FILE = "/usr/bin/cloc"
 
 
 def test_cloc_package_installed(host):
+    """
+    Tests if cloc package is in installed state.
+    """
     assert host.package(CLOC_APP).is_installed
 
 
 def test_cloc_binary_exists(host):
+    """
+    Tests if cloc binary exists.
+    """
     assert host.file(CLOC_BINARY_FILE).exists
 
 
 def test_cloc_binary_file(host):
+    """
+    Tests if cloc binary is a file type.
+    """
     assert host.file(CLOC_BINARY_FILE).is_file
 
 
 def test_cloc_binary_which(host):
+    """
+    Tests the output to confirm cloc's binary location.
+    """
     assert host.check_output('which cloc') == CLOC_BINARY_FILE
